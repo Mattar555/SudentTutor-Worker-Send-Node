60e4484f3edd6232503c8bbce372ae0814e188b9
Marwan <marwan@Marwans-MacBook-Pro.local>
2021-05-09 18:09:28 +1000

    Removed graph dependencies

--- src/main/java/com/hmses/demo/repository/GraphRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hmses.demo.repository;
-
-import com.hmses.demo.domain.graph.GraphCredentials;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface GraphRepository extends MongoRepository<GraphCredentials, String> {
-}
